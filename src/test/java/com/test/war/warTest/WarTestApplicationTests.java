6dc43749831e5a9a11b0d4878810f2179d3c6375
Nimje <nimashwi@amazon.com>
2023-08-16 18:38:03 -0700

    Seconf Commit

---
+++ src/test/java/com/test/war/warTest/WarTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.war.warTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
